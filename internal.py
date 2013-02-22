54d29607cc606143f00fe6e13e4ec26befe7d943
Adrian Kuendig <adriankue@gmail.com>
2013-02-22 10:06:25 +0100

    Scheduler done

--- internal.py
+++ internal.py
@@ -1,4 +1,11 @@
+from datetime import datetime
+
 def noop(*args, **kwargs): pass
+def identity(x): return x
+def defaultNow(): return int(datetime.now().timestamp() * 1000)
+def defaultComparer(x, y): return x == y
+def defaultSubComparer(x, y): return x - y
+def defaultKeySerializer(x): return str(x)
 def defaultError(error, *args, **kwargs): raise error
 
 def errorIfDisposed(disposable):
--- scheduler.py
+++ scheduler.py
@@ -1,19 +1,57 @@
 from .disposable import Disposable, CompositeDisposable
+from .functional import bind
+from .internal import defaultNow
+from threading import Timer
+
+class PeriodicTimerWithState:
+  def __init__(self, interval, function, state):
+    self.interval = interval
+    self.function = function
+    self.args = state
+
+  def _scheduled(self, run = True):
+    if run:
+      self.state = self.function(self.state)
+
+    #race condition
+    self.timer = Timer(self.interval, self._scheduled)
+
+  def start(self):
+    self._scheduled(False)
+
+    #race condition
+    return Disposable.create(lambda: self.timer.cancel())
+
 
 class RecursiveScheduledFunction:
-  def __init__(self, action, scheduler):
+  def __init__(self, action, scheduler, method = None):
     self.action = action
-    self.scheduler = scheduler
+    self.schedule = scheduler if method == None else scheduler[method]
     self.group = CompositeDisposable()
+
+    if method == None:
+      self.schedule = scheduler.scheduleWithState
+    else:
+      self.schedule = bind(getattr(scheduler, method), scheduler)
 
   def run(self, state):
     self.action(state, self.actionCallback)
 
-  def actionCallback(self, newState):
+  def actionCallback(self, newState, dueTime = None):
     self.isDone = False
     self.isAdded = False
 
-    self.cancel = self.scheduler.scheduleWithState(newState, self.schedulerCallback)
+    if dueTime == None:
+      self.cancel = self.schedule(
+        newState,
+        self.schedulerCallback
+      )
+    else:
+      self.cancel = self.schedule(
+        newState,
+        dueTime,
+        self.schedulerCallback
+      )
 
     # Assuming no concurrent access otherwise we would have
     # a race for self.group, self.isAdded and self.isDone
@@ -42,6 +80,7 @@ class Scheduler(object):
     self._scheduleRelative = scheduleRelative
     self._scheduleAbsolute = scheduleAbsolute
 
+  @staticmethod
   def invokeRecImmediate(scheduler, pair):
     state = pair[0]
     action = pair[1]
@@ -50,3 +89,95 @@ class Scheduler(object):
     scheduled.run(state)
 
     return scheduled.group
+
+  @staticmethod
+  def invokeRecDate(scheduler, pair, method):
+    state = pair[0]
+    action = pair[1]
+
+    scheduled = RecursiveScheduledFunction(action, scheduler, method)
+    scheduled.run(state)
+
+    return scheduled.group
+
+  @staticmethod
+  def invokeAction(scheduler, action):
+    action()
+    return Disposable.empty()
+
+  def catchException(self, handler):
+    return CatchScheduler(self, handler)
+
+  def schedulePeriodic(self, period, action):
+    return self.schedulerPeriodicWithState(None, period, lambda s: action())
+
+  def schedulePeriodicWithState(self, state, period, action):
+    timer = PeriodicTimerWithState(period, action, state)
+    return timer.start()
+
+  #Suspiciouse
+  def schedule(self, action):
+    return self._schedule(action, Scheduler.invokeAction)
+
+  def scheduleWithState(self, state, action):
+    return self._schedule(state, action)
+
+  def scheduleWithRelative(self, dueTime, action):
+    return self._scheduleRelative(action, dueTime, Scheduler.invokeAction)
+
+  def scheduleWithRelativeAndState(self, state, dueTime, action):
+    return self._scheduleRelative(state, dueTime, action)
+
+  def scheduleWithAbsolute(self, dueTime, action):
+    return self._scheduleAbsolute(action, dueTime, Scheduler.invokeAction)
+
+  def scheduleWithAbsoluteAndState(self, state, dueTime, action):
+    return self._scheduleAbsolute(state, dueTime, action)
+
+  def scheduleRecursive(self, action):
+    return self.scheduleRecursiveWithState(
+      action,
+      lambda _action, _self: _action(lambda: _self(_action)
+    ))
+
+  def scheduleRecursiveWithState(self, state, action):
+    return self.scheduleWithState((state, action), Scheduler.invokeRecImmediate)
+
+  def scheduleRecursiveWithRelative(self, dueTime, action):
+    return self.scheduleRecursiveWithRelativeAndState(
+      action,
+      dueTime,
+      lambda _action, _self: _action(lambda dt: _self(_action, dt)
+    ))
+
+  def scheduleRecursiveWithRelativeAndState(self, state, dueTime, action):
+    return self._scheduleRelative(
+      (state, action),
+      dueTime,
+      lambda s, p: Scheduler.invokeRecDate(s, p, 'scheduleWithRelativeAndState'
+    ))
+
+  def scheduleRecursiveWithAbsolute(self, dueTime, action):
+    return self.scheduleRecursiveWithAbsoluteAndState(
+      action,
+      lambda _action, _self: _action(lambda: _self(_action)
+    ))
+
+  def scheduleRecursiveWithAbsoluteAndState(self, state, dueTime, action):
+    return self._scheduleRelative(
+      (state, action),
+      dueTime,
+      lambda s, p: Scheduler.invokeRecDate(s, p, 'scheduleWithAbsoluteAndState'
+    ))
+
+  @staticmethod
+  def now():
+    return defaultNow()
+
+  @staticmethod
+  def normalize(timeSpan):
+    if timeSpan < 0:
+      return 0
+    else:
+      return timeSpan
+
