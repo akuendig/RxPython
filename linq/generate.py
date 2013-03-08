0919e815dc34827cb5626e39a1c5ad88cae45421
Adrian Kuendig <adriankue@gmail.com>
2013-03-08 20:20:56 +0100

    Added Generate, GetIterator

---
+++ linq/generate.py
@@ -0,0 +1,188 @@
+from disposable import Disposable
+from observable import Producer
+from .sink import Sink
+
+
+class Generate(Producer):
+  def __init__(self, initialState, condition, iterate, resultSelector, scheduler, timeSelector=None, isAbsolute=False):
+    self.initialState = initialState
+    self.condition = condition
+    self.iterate = iterate
+    self.resultSelector = resultSelector
+    self.scheduler = scheduler
+    self.timeSelector = timeSelector
+    self.isAbsolute = isAbsolute
+
+  def run(self, observer, cancel, setSink):
+    if self.isAbsolute and self.timeSelector != None:
+      sink = self.AlphaSink(self, observer, cancel)
+      setSink(sink)
+      return sink.run()
+    elif not self.isAbsolute and self.timeSelector != None:
+      sink = self.DeltaSink(self, observer, cancel)
+      setSink(sink)
+      return sink.run()
+    else:
+      sink = self.Sink(self, observer, cancel)
+      setSink(sink)
+      return sink.run()
+
+  class AlphaSink(Sink):
+    def __init__(self, parent, observer, cancel):
+      super(Generate.AlphaSink, self).__init__(observer, cancel)
+      self.parent = parent
+
+    def run(self):
+      self.first = True
+      self.hasResult = False
+      self.result = None
+
+      return self.parent.scheduler.scheduleWithState(
+        self.parent.initialState,
+        self.invokeRec
+      )
+
+    def invokeRec(self, scheduler, state):
+      time = 0
+
+      if self.hasResult:
+        self.observer.onNext(self.result)
+
+      try:
+        if self.first:
+          self.first = False
+        else:
+          state = self.parent.iterate(state)
+
+        self.hasResult = self.parent.condition(state)
+
+        if self.hasResult:
+          self.result = self.parent.resultSelector(state)
+          time = self.parent.timeSelector(state)
+      except Exception as e:
+        self.observer.onError(e)
+        self.dispose()
+        return Disposable.empty()
+
+      if not self.hasResult:
+        self.observer.onCompleted()
+        self.dispose()
+        return Disposable.empty()
+
+      return self.scheduleWithAbsoluteAndState(state, time, self.invokeRec)
+
+
+  class DeltaSink(Sink):
+    def __init__(self, parent, observer, cancel):
+      super(Generate.DeltaSink, self).__init__(observer, cancel)
+      self.parent = parent
+
+    def run(self):
+      self.first = True
+      self.hasResult = False
+      self.result = None
+
+      return self.parent.scheduler.scheduleWithState(
+        self.parent.initialState,
+        self.invokeRec
+      )
+
+    def invokeRec(self, scheduler, state):
+      time = 0
+
+      if self.hasResult:
+        self.observer.onNext(self.result)
+
+      try:
+        if self.first:
+          self.first = False
+        else:
+          state = self.parent.iterate(state)
+
+        self.hasResult = self.parent.condition(state)
+
+        if self.hasResult:
+          self.result = self.parent.resultSelector(state)
+          time = self.parent.timeSelector(state)
+      except Exception as e:
+        self.observer.onError(e)
+        self.dispose()
+        return Disposable.empty()
+
+      if not self.hasResult:
+        self.observer.onCompleted()
+        self.dispose()
+        return Disposable.empty()
+
+      return self.scheduleWithRelativeAndState(state, time, self.invokeRec)
+
+  class Sink(Sink):
+    def __init__(self, parent, observer, cancel):
+      super(Generate.Sink, self).__init__(observer, cancel)
+      self.parent = parent
+
+    def run(self):
+      self.state = self.parent.initialState
+      self.first = True
+
+      scheduler = self.parent.scheduler
+      if scheduler.isLongRunning:
+        return scheduler.scheduleLongRunning(self.loop)
+      else:
+        return scheduler.schedule(self.loopRec)
+
+    def loop(self, cancel):
+      while not cancel.isDisposed:
+        hasResult = False
+        result = None
+
+        try:
+          if self.first:
+            self.first = False
+          else:
+            self.state = self.parent.iterate(self.state)
+
+          hasResult = self.parent.condition(self.state)
+
+          if hasResult:
+            result = self.parent.resultSelector(self.state)
+        except Exception as e:
+          self.observer.onError(e)
+          self.dispose()
+          return
+
+        if hasResult:
+          self.observer.onNext(result)
+        else:
+          break
+
+      if not cancel.isDisposed:
+        self.observer.onCompleted()
+
+      self.dispose()
+
+    def loopRec(self, recurse):
+      hasResult = False
+      result = None
+
+      try:
+        if self.first:
+          self.first = False
+        else:
+          self.state = self.parent.iterate(self.state)
+
+        hasResult = self.parent.condition(self.state)
+
+        if hasResult:
+          result = self.parent.resultSelector(self.state)
+      except Exception as e:
+        self.observer.onError(e)
+        self.dispose()
+        return
+
+      if hasResult:
+        self.observer.onNext(result)
+        recurse()
+      else:
+        self.observer.onCompleted()
+        self.dispose()
---
+++ linq/getIterator.py
@@ -0,0 +1,95 @@
+from disposable import SingleAssignmentDisposable
+from observer import Observer
+from .sink import Sink
+from queue import Empty, Queue
+from threading import Semaphore
+
+
+class GetIterator(Observer):
+  def __init__(self):
+    self.queue = Queue()
+    self.gate = Semaphore(0)
+    self.subscription = SingleAssignmentDisposable()
+    self.error = None
+    self.done = False
+    self.disposed = False
+
+  def run(self, source):
+    # [OK] Use of unsafe Subscribe: non-pretentious exact mirror with the dual GetEnumerator method.
+    self.subscription.disposable = source.subscribe(self)
+    return self
+
+  def onNext(self, value):
+    self.queue.put_nowait(value)
+    self.gate.release()
+
+  def onError(self, exception):
+    self.error = exception
+    self.subscription.dispose()
+    self.gate.release()
+
+  def onCompleted(self):
+    self.done = True
+    self.subscription.dispose()
+    self.gate.release()
+
+  def __next__(self):
+    self.gate.acquire()
+
+    if self.disposed:
+      raise Exception("Object already disposed")
+
+    try:
+      return self.queue.get(True, 0)
+    except Empty:
+      pass
+
+    if self.error != None:
+      raise self.error
+
+    assert self.done
+
+    self.gate.release()
+
+    raise StopIteration()
+
+  def dispose(self):
+    self.subscription.dispose()
+    self.disposed = True
+    self.gate.release()
+
+  class Sink(Sink):
+    def __init__(self, parent, observer, cancel):
+      super(GetIterator.Sink, self).__init__(observer, cancel)
+      self.parent = parent
+      self.currentKey = None
+      self.hasCurrentKey = False
+
+    def onNext(self, value):
+      try:
+        key = self.parent.keySelector(value)
+      except Exception as e:
+        self.observer.onError(e)
+        self.dispose()
+      else:
+        equal = False
+
+        if self.hasCurrentKey:
+          try:
+            equal = self.currentKey == key
+          except Exception as e:
+            self.observer.onError(e)
+            self.dispose()
+
+        if not self.hasCurrentKey or not equal:
+          self.hasCurrentKey = True
+          self.currentKey = key
+          self.observer.onNext(value)
+
+    def onError(self, exception):
+      self.observer.onError(exception)
+      self.dispose()
+
+    def onCompleted(self):
+      self.observer.onCompleted()
+      self.dispose()
