dccaa379decc856ca9bb2f4dbe600b89a9b352be
Adrian Kuendig <adriankue@gmail.com>
2013-03-08 14:59:52 +0100

    added Dematerialize and Distinct

---
+++ linq/dematerialize.py
@@ -0,0 +1,37 @@
+from observable import Producer
+from notification import Notification
+from .sink import Sink
+
+
+class Dematerialize(Producer):
+  def __init__(self, source):
+    self.source = source
+
+  def run(self, observer, cancel, setSink):
+    sink = self.Sink(observer, cancel)
+    setSink(sink)
+    return self.source.subscribeSafe(sink)
+
+  class Sink(Sink):
+    def __init__(self, observer, cancel):
+      super(Dematerialize.Sink, self).__init__(observer, cancel)
+
+    def onNext(self, value):
+      if value.kind == Notification.KIND_NEXT:
+        self.observer.onNext(value.value)
+      elif value.kind == Notification.KIND_ERROR:
+        self.observer.onError(value.exception)
+        self.dispose()
+      elif value.kind == Notification.KIND_COMPLETED:
+        self.observer.onCompleted()
+        self.dispose()
+      else:
+        self.observer.onError(Exception("Unknown notification kind %s"%value.kind))
+
+    def onError(self, exception):
+      self.observer.onError(exception)
+      self.dispose()
+
+    def onCompleted(self):
+      self.observer.onCompleted()
+      self.dispose()
---
+++ linq/distinct.py
@@ -0,0 +1,40 @@
+from observable import Producer
+from .sink import Sink
+
+
+class Distinct(Producer):
+  def __init__(self, source, keySelector=id):
+    self.source = source
+    self.keySelector = keySelector
+
+  def run(self, observer, cancel, setSink):
+    sink = self.Sink(self, observer, cancel)
+    setSink(sink)
+    return self.source.subscribeSafe(sink)
+
+  class Sink(Sink):
+    def __init__(self, parent, observer, cancel):
+      super(Distinct.Sink, self).__init__(observer, cancel)
+      self.parent = parent
+      self.hashSet = set()
+
+    def onNext(self, value):
+      try:
+        key = self.parent.keySelector(value)
+      except Exception as e:
+        self.observer.onError(e)
+        self.dispose()
+      else:
+        if key in self.hashSet:
+          return
+        else:
+          self.hashSet.add(key)
+          self.observer.onNext(value)
+
+    def onError(self, exception):
+      self.observer.onError(exception)
+      self.dispose()
+
+    def onCompleted(self):
+      self.observer.onCompleted()
+      self.dispose()
